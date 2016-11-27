daaf14c322c7ca250fecc12e9d127c609fbff1b1
Samuel Shifterovich <Shifterovich@users.noreply.github.com>
2016-11-27 17:42:20 +0100

    Update news.js

--- etc/news.js
+++ etc/news.js
@@ -1,19 +1,19 @@
 /* only 5 news per page */
 
-news0 = "<h2>0test</h2>new content&nbsp;";
-news1 = "<h2>1test</h2>new content&nbsp;";
-news2 = "<h2>2test</h2>new content&nbsp;";
-news3 = "<h2>3test</h2>new content&nbsp;";
-news4 = "<h2>4test</h2>new content&nbsp;";
-news5 = "<h2>5test</h2>new content&nbsp;";
-news6 = "<h2>6test</h2>new content&nbsp;";
+news0 = "<h2>0test</h2>new content";
+news1 = "<h2>1test</h2>new content";
+news2 = "<h2>2test</h2>new content";
+news3 = "<h2>3test</h2>new content";
+news4 = "<h2>4test</h2>new content";
+news5 = "<h2>5test</h2>new content";
+news6 = "<h2>6test</h2>new content";
 
 var news = [
-	news0+"<a href='post.html#0'>#0</a>",
-	news1+"<a href='post.html#1'>#1</a>",
-	news2+"<a href='post.html#2'>#2</a>",
-	news3+"<a href='post.html#3'>#3</a>",
-	news4+"<a href='post.html#4'>#4</a>",
-	news5+"<a href='post.html#5'>#5</a>",
-	news6+"<a href='post.html#6'>#6</a>"
+	news0+"&nbsp;<a href='post.html#0'>#0</a>",
+	news1+"&nbsp;<a href='post.html#1'>#1</a>",
+	news2+"&nbsp;<a href='post.html#2'>#2</a>",
+	news3+"&nbsp;<a href='post.html#3'>#3</a>",
+	news4+"&nbsp;<a href='post.html#4'>#4</a>",
+	news5+"&nbsp;<a href='post.html#5'>#5</a>",
+	news6+"&nbsp;<a href='post.html#6'>#6</a>"
 ];
